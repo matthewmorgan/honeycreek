72adf808fc8afeb0bbd1163938b327961328f585
Matthew Morgan <matt@dulcetsoftware.com>
2015-10-30 18:46:01 -0400

    lint cleanup

--- public/js/main.js
+++ public/js/main.js
@@ -4,30 +4,30 @@ var userId = 'unknown';
 var isSubmitting = false;
 
 
-var updatePhotoCaption = function(imageId, captionText){
-  var captionBox = $('.gallery-row').find('#caption-'+imageId);
+var updatePhotoCaption = function (imageId, captionText) {
+  var captionBox = $('.gallery-row').find('#caption-' + imageId);
   captionBox.fadeTo("fast", 0.33);
   var public_id = imageId.split('-')[1],
       tags = [
-        'user-'+userId,
-        'caption-'+captionText.split(',').join(' ')
+        'user-' + userId,
+        'caption-' + captionText.split(',').join(' ')
       ];
 
   var paramString = '?public_id='+public_id+'&tags='+encodeURIComponent(tags);
 
   superagent
-  .put('http://honey-server.apps.dulcetsoftware.com/cloudinary/updatecaption'+paramString)
-  .end(function (err, result){
+      .put('http://honey-server.apps.dulcetsoftware.com/cloudinary/updatecaption'+paramString)
+      .end(function (err) {
         if (err) throw err;
         captionBox.fadeTo("fast", 1.0);
       })
 };
 
 
-var buildArticle = function (imageHtml, text) {
+var buildArticle = function (imageHtml) {
   var imageId = 'img-' + (imageHtml.match("[a-z0-9]+\.(jpg|png|bmp|gif|tif)")[0].split('.')[0]);
   $('.gallery-row').on('change', '#caption-' + imageId, function () {
-    var captionText=$('#caption-'+imageId).val().trim();
+    var captionText = $('#caption-' + imageId).val().trim();
     updatePhotoCaption(imageId, captionText);
   });
   return '<div class="4u 12u(mobile)">' +
@@ -45,11 +45,12 @@ var attachUploadWidget = function () {
         thumbnail_transformation: {width: 383, height: 383, crop: 'fill'},
         tags:                     'user-' + userId
       },
-      function (error, result) {
+      function (err) {
+        if (err) throw err;
         var galleryHtml = "";
         $('.cloudinary-thumbnails .cloudinary-thumbnail').each(function () {
           var imageHtml = $(this).html();
-          galleryHtml += (buildArticle(imageHtml, "Add a caption here!"))
+          galleryHtml += (buildArticle(imageHtml))
         });
         $('.gallery-row').html(galleryHtml);
         $('#photomessage').text("Thanks for sharing your photos!");
@@ -62,12 +63,12 @@ var revealImageUploadButton = function () {
   $('.upload_widget_wrapper').removeClass('hidden');
 };
 
-var validateFormData = function(){
+var validateFormData = function () {
   return true;
 };
 
 var storeUserData = function (data, callback) {
-  data.email=data.email.toLowerCase();
+  data.email = data.email.toLowerCase();
   superagent
       .post('http://honey-server.apps.dulcetsoftware.com/user')
       .send(data)
@@ -75,65 +76,67 @@ var storeUserData = function (data, callback) {
         if (err) {
           alert('There was an error storing your information!');
           throw err;
-        };
+        }
         callback(result);
       })
 
 };
 
-var fetchThreeImages = function(callback) {
+var fetchThreeImages = function (callback) {
   superagent
-  .get('http://honey-server.apps.dulcetsoftware.com/cloudinary/randomimages/3')
-  .end(function(err, result){
+      .get('http://honey-server.apps.dulcetsoftware.com/cloudinary/randomimages/3')
+      .end(function (err, result) {
         if (err) throw err;
         result = JSON.parse(result.text);
-        var imageArray = result.reduce(function(acc, resource){
-          acc.push({ url: resource.url, tags: resource.tags });
+        var imageArray = result.reduce(function (acc, resource) {
+          acc.push({url: resource.url, tags: resource.tags});
           return acc;
         }, []);
         callback(imageArray);
       })
 };
 
-var hideRegistrationForm = function(){
+var hideRegistrationForm = function () {
   $('.fadable').fadeOut();
 };
 
 var attachFormHandler = function () {
   $('#form-submit-button').click(function (event) {
     event.preventDefault();
-    if (!isSubmitting && validateFormData()){
-      var data = {};
-      $("#contact-form").serializeArray().map(function(x){data[x.name] = x.value;});
+    if (!isSubmitting && validateFormData()) {
+      var data = $("#contact-form").serializeArray().reduce(function (acc, x) {
+        acc[x.name] = x.value;
+        return acc;
+      },{});
 
       storeUserData(data, function (result) {
         userId = JSON.parse(result.text).value._id;
         attachUploadWidget();
         hideRegistrationForm();
         revealImageUploadButton();
-        isSubmitting=false;
+        isSubmitting = false;
       })
     }
   });
 };
 
 
-var displayImagesInGallery = function(displayedImageElements, displayedCaptions, imageArray){
-  if (imageArray.length<3) return false;
-  imageArray.forEach(function(image, index){
+var displayImagesInGallery = function (displayedImageElements, displayedCaptions, imageArray) {
+  if (imageArray.length < 3) return false;
+  imageArray.forEach(function (image, index) {
     displayOne(displayedImageElements, displayedCaptions, image, index);
   });
   $('.thumbnails').fadeTo("fast", 1.0);
 };
 
-var displayOne = function(displayedImageElements, displayedCaptions, image, index){
-  var caption = image.tags.filter(function(tag){
-    return tag.substring(0,7)==='caption';
-  })[0].substring(8) || "";
+var displayOne = function (displayedImageElements, displayedCaptions, image, index) {
+  var caption = image.tags.filter(function (tag) {
+        return tag.substring(0, 7) === 'caption';
+      })[0].substring(8) || "";
 
   var partials = image.url.split('upload');
-  var rightSizeUrl = partials[0]+'upload/c_fill,h_335,w_335'+partials[1];
-  $(displayedImageElements[index]).attr('src',rightSizeUrl);
+  var rightSizeUrl = partials[0] + 'upload/c_fill,h_335,w_335' + partials[1];
+  $(displayedImageElements[index]).attr('src', rightSizeUrl);
   $(displayedCaptions[index]).text(caption)
 };
 
@@ -142,7 +145,9 @@ $(document).ready(function () {
   $('html').fadeTo("slow", 1.0);
 
   attachFormHandler();
-  fetchThreeImages(function(result){
-    displayImagesInGallery($('.randomimage'),$('.randomcaption'),result);
+  fetchThreeImages(function (randomImages) {
+    var imagePlaceholders = $('.randomimage');
+    var captionPlaceholders = $('.randomcaption');
+    displayImagesInGallery(imagePlaceholders, captionPlaceholders, randomImages);
   })
 });
