53b31d67906e17c3cb90e9ca4e3e8e5fa1996103
Matthew Morgan <matt@dulcetsoftware.com>
2015-10-31 18:24:04 -0400

    old cors

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var cors = require('cors');
+//var cors = require('cors');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -14,29 +14,31 @@ var moderate = require('./routes/moderate');
 //var data = require('./routes/users');
 
 var app = express();
-//var allowCrossDomain = function(req, res, next) {
-//
-//  res.header('Access-Control-Allow-Origin', '*');
-//  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//  res.header('Access-Control-Allow-Credentials', false);
-//  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-//
-//  // intercept OPTIONS method
-//  if ('OPTIONS' == req.method) {
-//    res.send(200);
-//    res.end();
-//  }
-//  else {
-//    next();
-//  }
-//};
+var allowCrossDomain = function(req, res, next) {
+
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Credentials', false);
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+    res.end();
+  }
+  else {
+    next();
+  }
+};
 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+app.use(allowCrossDomain);
 // uncomment after placing your favicon in /public
-app.use(cors());
+//app.use(cors());
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.use(logger('dev'));
@@ -45,8 +47,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', cors(), index);
-app.use('/moderate', cors(), moderate);
+app.use('/', index);
+app.use('/moderate', moderate);
 
 
 // catch 404 and forward to error handler
