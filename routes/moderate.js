c69a05f307161d12ed92493d42e5b2f1b92ebfeb
Matthew Morgan <matt@dulcetsoftware.com>
2015-11-17 07:10:38 -0500

    provide route to list registrant data;

--- routes/moderate.js
+++ routes/moderate.js
@@ -14,10 +14,32 @@ router.get('/', function (req, res, next) {
       })
 });
 
+router.get('/registrants', function(req, res, next){
+  superagent
+      .get('http://honey-server.apps.dulcetsoftware.com/users')
+      .end((err, result) => {
+        if (err) throw err;
+        var users = result.body;
+        users.sort(sortUsersByAttending);
+        res.render('registrants', {users:users});
+      })
+});
+
 var sortUsersByApprovedMessages = function(user1, user2){
   //unapproved before approved
   if (user1.messageApproved){
     if (!user2.messageApproved){
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+  return -1;
+};
+
+var sortUsersByAttending = function(user1, user2){
+  if (user1.isAttending){
+    if(!user2.isAttending){
       return 1;
     } else {
       return 0;
