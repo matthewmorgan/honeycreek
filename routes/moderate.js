5443f65060b72ccf219a60fc62eec3f956ff0f5c
Matthew Morgan <matt@dulcetsoftware.com>
2016-01-01 17:53:18 -0500

    Refactor comparator

--- routes/moderate.js
+++ routes/moderate.js
@@ -2,28 +2,27 @@ const router = require('express').Router();
 const superagent = require('superagent');
 
 
-router.get('/',  (req, res, next) => {
+router.get('/', (req, res) => {
   superagent
       .get('http://honey-server.apps.dulcetsoftware.com/users')
       .end((err, result) => {
         if (err) throw err;
         const users = result.body;
         users.sort(sortUsersByApprovedMessages);
-        res.render('moderate', {users:users});
+        res.render('moderate', {users: users});
       })
 });
 
-function sortUsersByApprovedMessages(user1, user2){
+function sortUsersByApprovedMessages(user1, user2) {
   //unapproved before approved
-  if (user1.messageApproved){
-    if (!user2.messageApproved){
-      return 1;
-    } else {
-      return 0;
-    }
+  if (user1.messageApproved && !user2.messageApproved) {
+    return 1;
+  } else if (user1.messageApproved) {
+    return 0;
+  } else {
+    return -1;
   }
-  return -1;
-};
+}
 
 module.exports = router;
 
