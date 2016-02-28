eabc50248adde7fd295fc4e9a0731ed3b0091e7a
Matthew Morgan <matt@dulcetsoftware.com>
2016-02-28 07:29:07 -0500

    Require cookie for registrants route

--- routes/registrants.js
+++ routes/registrants.js
@@ -2,20 +2,25 @@ const router = require('express').Router();
 const superagent = require('superagent');
 
 router.get('/', (req, res, next) => {
-  superagent
-      .get('http://honey-server.apps.dulcetsoftware.com/users')
-      .end((err, result) => {
-        if (err) throw err;
-        const users = result.body;
-        users.sort(sortUsersByAttending);
-        res.render('registrants', {users:users});
-      })
+  const cookies = req.cookies;
+  if (cookies.authenticated_username) {
+    superagent
+        .get('http://honey-server.apps.dulcetsoftware.com/users')
+        .end((err, result) => {
+          if (err) throw err;
+          const users = result.body;
+          users.sort(sortUsersByAttending);
+          res.render('registrants', {users: users});
+        })
+  } else {
+    res.send(403);
+  }
 });
 
 
-function sortUsersByAttending (user1, user2) {
-  if (user1.isAttending === 'true'){
-    if(user2.isAttending === 'false'){
+function sortUsersByAttending(user1, user2) {
+  if (user1.isAttending === 'true') {
+    if (user2.isAttending === 'false') {
       return 1;
     } else {
       return user1.name.localeCompare(user2.name);
