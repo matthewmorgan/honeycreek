78062fe268e798756ad3416fb70d42de7938cd83
Matthew Morgan <matt@dulcetsoftware.com>
2016-03-11 22:17:59 -0500

    Remove shuffle

--- public/js/main.js
+++ public/js/main.js
@@ -158,27 +158,12 @@ function showComments(comments) {
   });
 }
 
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-
-  while (0 !== currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-  return array;
-}
-
 function loadComments(showComments) {
   superagent
       .get(serverAddress + '/comments/all')
       .end(function (err, result) {
         if (err) throw err;
         result = result.text;
-        result = shuffle(result);
         showComments(result);
       })
 }
