fa2cd5addf5f628fb9c5ef6f1f9e0b7ecfe10319
Matthew Morgan <matt@dulcetsoftware.com>
2016-05-14 08:34:45 -0400

    Wait until dom ready for gallery js

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,6 +1,7 @@
-var serverAddress = 'http://honey-server.apps.dulcetsoftware.com';
+$(document).ready(function () {
+  var serverAddress = 'http://honey-server.apps.dulcetsoftware.com';
 
-var pswpElement = document.querySelectorAll('.pswp')[0];
+  var pswpElement = document.querySelectorAll('.pswp')[0];
 
 // build items array
 //var items = [
@@ -17,48 +18,50 @@ var pswpElement = document.querySelectorAll('.pswp')[0];
 //];
 
 
-function initGallery(images){
-  // define options (if needed)
-  var options = {
-    // optionName: 'option value'
-    // for example:
-    index: 0 // start at first slide
-  };
+  function initGallery(images) {
+    // define options (if needed)
+    var options = {
+      // optionName: 'option value'
+      // for example:
+      index: 0 // start at first slide
+    };
 
 // Initializes and opens PhotoSwipe
-  var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, images, options);
-  gallery.init();
-}
+    var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, images, options);
+    gallery.init();
+  }
 
-function fetchAllImages(callback) {
-  superagent
-      .get(serverAddress + '/cloudinary/imagegallery')
-      .end(function (err, result) {
-        if (err) throw err;
-        result = JSON.parse(result.text);
-        console.log(result);
-        var imageArray = result.reduce(function (acc, resource) {
-          acc.push({
-            src: resource.secure_url,
-            w: 600,
-            h: 600,
-            title: extractCaption(resource)
-          });
-          return acc;
-        }, []);
-        callback(imageArray);
-      })
-}
+  function fetchAllImages(callback) {
+    superagent
+        .get(serverAddress + '/cloudinary/imagegallery')
+        .end(function (err, result) {
+          if (err) throw err;
+          result = JSON.parse(result.text);
+          console.log(result);
+          var imageArray = result.reduce(function (acc, resource) {
+            acc.push({
+              src:   resource.secure_url,
+              w:     600,
+              h:     600,
+              title: extractCaption(resource)
+            });
+            return acc;
+          }, []);
+          callback(imageArray);
+        })
+  }
 
-fetchAllImages(initGallery);
+  fetchAllImages(initGallery);
 
-function extractCaption(image){
-  var tags = image['tags'];
-  var caption ='';
-  tags.forEach((tag) => {
-    if (tag.match('^caption-')){
-      caption = tag.split('').splice(8).join('');
-    }
-  });
-  return caption;
-}
+  function extractCaption(image) {
+    var tags = image['tags'];
+    var caption = '';
+    tags.forEach((tag) => {
+      if (tag.match('^caption-')) {
+        caption = tag.split('').splice(8).join('');
+      }
+    });
+    return caption;
+  }
+
+});
