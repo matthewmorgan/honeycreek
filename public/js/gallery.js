e298443707a8dd9a6fcc890b6421bfba66296e96
Matthew Morgan <matt@dulcetsoftware.com>
2016-05-15 08:39:44 -0400

    Implement Galleria

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,72 +1,81 @@
+var serverAddress = 'http://honey-server.apps.dulcetsoftware.com';
+
+function fetchAllImages(callback) {
+  superagent
+      .get(serverAddress + '/cloudinary/imagegallery')
+      .end(function (err, result) {
+        if (err) throw err;
+        result = JSON.parse(result.text);
+        var imageNames = [];
+        var imageArray = result.reduce(function (acc, resource) {
+          acc.push({
+            link:   resource.secure_url,
+            image: resource.secure_url,
+            description: extractCaption(resource)
+          });
+          imageNames.push(extractName(resource.secure_url));
+          return acc;
+        }, []);
+        callback(imageArray);
+      })
+}
+
+function extractName(url){
+  var frags = url.split('/');
+  return frags[frags.length-1].split('.')[0];
+}
+
+function deriveThumb(url){
+  var frags = url.split('/');
+  var targetIndex = frags.length-2;
+  frags[targetIndex] = 'w_100, h_100, c_fill';
+  return frags.join('/');
+}
+
+function deriveBig(url){
+  var frags = url.split('/');
+  var targetIndex = frags.length-2;
+  frags[targetIndex] = 'w_1000, h_1000, c_fill';
+  return frags.join('/');
+}
+
+function extractCaption(image) {
+  var tags = image['tags'];
+  var caption = '';
+  tags.forEach((tag) => {
+    if (tag.match('^caption-')) {
+      caption = tag.split('').splice(8).join('');
+    }
+  });
+  return caption;
+}
+
+
 $(document).ready(function () {
-  var serverAddress = 'http://honey-server.apps.dulcetsoftware.com';
 
-  var pswpElement = document.querySelectorAll('.pswp')[0];
+  //var data = [
+  //  {
+  //    image: 'img1.jpg',
+  //    thumb: 'thumb1.jpg',
+  //    big: 'big1.jpg',
+  //    title: 'my first image',
+  //    description: 'Lorem ipsum caption',
+  //    link: 'http://domain.com'
+  //  },
+  //  {
+  //    video: 'http://www.youtube.com/watch?v=GCZrz8siv4Q',
+  //    title: 'my second image',
+  //    description: 'Another caption'
+  //  }
+  //];
 
-  function initGallery(images, startingImageIndex) {
-    // define options (if needed)
-    var index = startingImageIndex || 0;
-    var options = {
-      // optionName: 'option value'
-      // for example:
-      index: index //0 is first slide
-    };
-
-// Initializes and opens PhotoSwipe
-    var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, images, options);
-    gallery.init();
-  }
-
-  function fetchAllImages(callback) {
-    superagent
-        .get(serverAddress + '/cloudinary/imagegallery')
-        .end(function (err, result) {
-          if (err) throw err;
-          result = JSON.parse(result.text);
-          console.log(result);
-          var imageNames = [];
-          var imageArray = result.reduce(function (acc, resource) {
-            acc.push({
-              src:   resource.secure_url,
-              w:     600,
-              h:     600,
-              title: extractCaption(resource)
-            });
-            imageNames.push(extractName(resource.secure_url));
-            return acc;
-          }, []);
-          var referringImageName = getReferringImageName();
-          console.log('referringImageName', referringImageName);
-          var referringImageId = getReferringImageIndex(referringImageName, imageNames);
-          console.log('referringImageId', referringImageId);
-          callback(imageArray, referringImageId);
-        })
-  }
-
-  function extractName(url){
-    var frags = url.split('/');
-    return frags[frags.length-1].split('.')[0];
-  }
-
-  function getReferringImageIndex(name, names){
-    return names.indexOf(name) >= 0 ? names.indexOf(name) : 0;
-  }
-
-  function getReferringImageName(){
-    return document.getElementById('referrer').dataset['name'] || '';
+  function initGallery(data){
+    Galleria.loadTheme('/js/galleria-classic-min.js');
+    Galleria.run('.galleria', {
+      dataSource: data
+    });
   }
 
   fetchAllImages(initGallery);
-
-  function extractCaption(image) {
-    var tags = image['tags'];
-    var caption = '';
-    tags.forEach((tag) => {
-      if (tag.match('^caption-')) {
-        caption = tag.split('').splice(8).join('');
-      }
-    });
-    return caption;
-  }
 
 });
